d4ed4d4e70872031e0030f122a5adda52b4bb16b
lstux <33700752+lstux@users.noreply.github.com>
2020-10-05 14:23:49 +0200

    Create QemuStatic-build.sh

---
+++ QemuStatic-build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+WORKDIR="$(realpath "$(dirname "${0}")")"
+BUILDDIR="${WORKDIR}/build"
+
+#Clone Qemu sources repository
+
+#Configure Qemu sources
+
+#Build Qemu static
+
+#Rename and get static binaries
+
+#Cleanup sources directory
